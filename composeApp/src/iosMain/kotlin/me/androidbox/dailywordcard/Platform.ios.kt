27edaa542e27036a7e10dcaf475abbbadb391c1a
steve1.rm <steve1.rm@gmail.com>
2025-05-11 15:10:48 +0100

    implemented TextReaderImp using AVSpeechSynthesizer for iOS

--- composeApp/src/iosMain/kotlin/me/androidbox/dailywordcard/Platform.ios.kt
+++ composeApp/src/iosMain/kotlin/me/androidbox/dailywordcard/Platform.ios.kt
@@ -10,11 +10,30 @@ class IOSPlatform: Platform {
 actual fun getPlatform(): Platform = IOSPlatform()
 
 actual class TextReaderImp() : TextReader {
+    private val synthesizer = AVSpeechSynthesizer()
+
     actual override fun readText(text: String) {
-        TODO("Not yet implemented")
+        // Create an AVSpeechUtterance with the text
+        val utterance = AVSpeechUtterance(string = text)
+
+        // Optionally, set the voice and other properties
+        // You can get a list of available voices with AVSpeechSynthesisVoice.speechVoices()
+        // For example, to use the default language voice:
+        utterance.voice = AVSpeechSynthesisVoice.voiceWithLanguage(NSLocale.currentLocale.languageCode)
+
+        // You can adjust rate, pitch, and volume:
+        // utterance.rate = 0.5 // Slower
+        // utterance.pitchMultiplier = 1.2 // Higher pitch
+        // utterance.volume = 0.8 // Lower volume
+
+        // Speak the utterance
+        synthesizer.speakUtterance(utterance)
     }
 
     actual override fun shutdown() {
-        TODO("Not yet implemented")
+        // Stop any ongoing speech
+        synthesizer.stopSpeakingAtBoundary(platform.AVFoundation.AVSpeechBoundary.AVSpeechBoundaryImmediate)
+        // AVSpeechSynthesizer doesn't have an explicit shutdown like Android's TextToSpeech.
+        // Releasing the instance and letting ARC handle it is sufficient.
     }
 }
